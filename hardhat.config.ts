be3547ac36d2327fafdc20f979a1301c9839a7a8
entin-hun <50300541+entin-hun@users.noreply.github.com>
2024-08-21 18:23:20 +0200

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,41 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-// Go to https://alchemy.com, sign up, create a new App in
-// its dashboard, and add its key to the configuration variables
-const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
-
-// Add your Sepolia account private key to the configuration variables
-// To export your private key from Coinbase Wallet, go to
-// Settings > Developer Settings > Show private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
-
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 2000,
-        details: {
-          yul: true,
-          yulDetails: {
-            stackAllocation: true,
-            optimizerSteps: "dhfoDgvulfnTUtnIf"
-          }
+  solidity: "v0.8.20", // replace if necessary
+  networks: {
+    'gnosis-chiado-testnet': {
+      url: 'https://rpc.chiadochain.net'
+    },
+  },
+  etherscan: {
+    apiKey: {
+      'gnosis-chiado-testnet': 'empty'
+    },
+    customChains: [
+      {
+        network: "gnosis-chiado-testnet",
+        chainId: 10200,
+        urls: {
+          apiURL: "https://gnosis-chiado.blockscout.com/api",
+          browserURL: "https://gnosis-chiado.blockscout.com"
         }
       }
-    }
-  },
-  networks: {
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY]
-    }
+    ]
   }
 };
-
-export default config;
